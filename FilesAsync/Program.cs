24e802a53407cc51eb26e323d92ce8d0284c3c53
Jachu-K <01199299@pw.edu.pl>
2025-12-11 11:59:38 +0100

    asdasd

---
+++ FilesAsync/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+var files = Directory.EnumerateFiles(".", "*.json");
+
+foreach (var file in files)
+{
+    using var stream = File.OpenRead(file);
+    var data = await JsonSerializer.DeserializeAsync<Data>(stream);
+    Console.WriteLine($"Deserialized: {data?.Id}");
+}
+
+public record Data(int Id, string? Name);
